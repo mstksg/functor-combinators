06560422fb9c171a61264cf66f27bbfcc7996c12
Justin Le <justin@jle.im>
2021-09-12 21:33:23 -0700

    the missing Inplicative instances

--- src/Data/Functor/Invariant/Inplicative.hs
+++ src/Data/Functor/Invariant/Inplicative.hs
@@ -35,55 +35,60 @@ module Data.Functor.Invariant.Inplicative (
   ) where
 
 import           Control.Applicative
-import           Control.Applicative.Backwards        (Backwards)
-import           Control.Applicative.Lift             (Lift(Pure))
-import           Control.Arrow                        (Arrow)
-import           Control.Comonad                      (Cokleisli)
-import           Control.Monad.Trans.Cont             (ContT)
-import           Control.Monad.Trans.Error            (ErrorT)
-import           Control.Monad.Trans.Except           (ExceptT)
-import           Control.Monad.Trans.Identity         (IdentityT)
-import           Control.Monad.Trans.List             (ListT)
-import           Control.Monad.Trans.Maybe            (MaybeT)
-import           Control.Monad.Trans.RWS              (RWST)
-import           Control.Monad.Trans.Reader           (ReaderT)
-import           Control.Monad.Trans.Writer           (WriterT)
+import           Control.Applicative.Backwards               (Backwards)
+import           Control.Applicative.Lift                    (Lift(Pure))
+import           Control.Arrow                               (Arrow)
+import           Control.Comonad                             (Cokleisli)
+import           Control.Monad.Trans.Cont                    (ContT)
+import           Control.Monad.Trans.Error                   (ErrorT(..))
+import           Control.Monad.Trans.Except                  (ExceptT(..))
+import           Control.Monad.Trans.Identity                (IdentityT)
+import           Control.Monad.Trans.List                    (ListT(..))
+import           Control.Monad.Trans.Maybe                   (MaybeT(..))
+import           Control.Monad.Trans.RWS                     (RWST(..))
+import           Control.Monad.Trans.Reader                  (ReaderT(..))
+import           Control.Monad.Trans.State                   (StateT)
+import           Control.Monad.Trans.Writer                  (WriterT(..))
 import           Control.Natural
-import           Data.Biapplicative                   (Biapplicative)
-import           Data.Complex                         (Complex)
+import           Data.Biapplicative                          (Biapplicative)
+import           Data.Complex                                (Complex)
 import           Data.Deriving
 import           Data.Functor.Apply
-import           Data.Functor.Bind.Class              (Biapply, Bind)
-import           Data.Functor.Compose                 (Compose)
-import           Data.Functor.Constant                (Constant)
+import           Data.Functor.Bind.Class                     (Biapply, Bind)
+import           Data.Functor.Constant                       (Constant)
+import           Data.Functor.Contravariant
 import           Data.Functor.Contravariant
 import           Data.Functor.Contravariant.Divise
 import           Data.Functor.Contravariant.Divisible
 import           Data.Functor.Identity
 import           Data.Functor.Invariant
 import           Data.Functor.Invariant.Day
-import           Data.Functor.Product                 (Product)
-import           Data.Functor.Reverse                 (Reverse)
-import           Data.Hashable                        (Hashable)
+import           Data.Functor.Product                        (Product)
+import           Data.Functor.Reverse                        (Reverse)
+import           Data.Hashable                               (Hashable)
 import           Data.Kind
-import           Data.List.NonEmpty                   (NonEmpty)
-import           Data.SOP hiding                      (hmap, Compose)
-import           Data.Semigroupoid.Static             (Static)
-import           Data.Sequence                        (Seq)
-import           Data.Tagged                          (Tagged)
-import           Data.Tree                            (Tree)
-import           GHC.Generics                         (Generic)
-import qualified Data.Bifunctor.Join                  as BFJ
-import qualified Data.HashMap.Lazy                    as HM
-import qualified Data.IntMap                          as IM
-import qualified Data.Map                             as M
-import qualified Data.Monoid                          as Monoid
-import qualified Data.Semigroup                       as Semigroup
-import qualified Data.Vinyl                           as V
-import qualified Data.Vinyl.Curry                     as V
-import qualified Data.Vinyl.Functor                   as V
-import qualified Data.Vinyl.XRec                      as V
-import qualified GHC.Generics                         as Generics
+import           Data.List.NonEmpty                          (NonEmpty)
+import           Data.SOP hiding                             (hmap)
+import           Data.Semigroupoid.Static                    (Static)
+import           Data.Sequence                               (Seq)
+import           Data.StateVar                               (SettableStateVar)
+import           Data.Tagged                                 (Tagged)
+import           Data.Tree                                   (Tree)
+import           GHC.Generics                                (Generic)
+import qualified Control.Monad.Trans.RWS.Strict as Strict    (RWST(..))
+import qualified Control.Monad.Trans.State.Strict as Strict  (StateT)
+import qualified Control.Monad.Trans.Writer.Strict as Strict (WriterT(..))
+import qualified Data.Bifunctor.Join                         as BFJ
+import qualified Data.HashMap.Lazy                           as HM
+import qualified Data.IntMap                                 as IM
+import qualified Data.Map                                    as M
+import qualified Data.Monoid                                 as Monoid
+import qualified Data.Semigroup                              as Semigroup
+import qualified Data.Vinyl                                  as V
+import qualified Data.Vinyl.Curry                            as V
+import qualified Data.Vinyl.Functor                          as V
+import qualified Data.Vinyl.XRec                             as V
+import qualified GHC.Generics                                as Generics
 
 -- | The invariant counterpart of 'Apply' and 'Divise'.
 --
@@ -179,22 +184,28 @@ dather
     => Day f f ~> f
 dather (Day x y a b) = gather a b x y
 
+-- | Ignores the contravariant part of 'gather'
 instance Apply f => Inply (WrappedFunctor f) where
     gather f _ (WrapFunctor x) (WrapFunctor y) = WrapFunctor (liftF2 f x y)
     gathered (WrapFunctor x) (WrapFunctor y) = WrapFunctor (liftF2 (,) x y)
+-- | @'knot' = 'pure'@
 instance (Applicative f, Apply f) => Inplicative (WrappedFunctor f) where
     knot = pure
 
+-- | Ignores the covariant part of 'gather'
 instance Divise f => Inply (WrappedContravariant f) where
     gather _ g (WrapContravariant x) (WrapContravariant y) = WrapContravariant (divise g x y)
     gathered (WrapContravariant x) (WrapContravariant y) = WrapContravariant (divised x y)
+-- | @'knot' _ = 'conquer'@
 instance (Divisible f, Divise f) => Inplicative (WrappedContravariant f) where
     knot _ = conquer
 
-instance Divisible f => Inply (WrappedDivisible f) where
-    gather _ g (WrapDivisible x) (WrapDivisible y) = WrapDivisible (divide g x y)
-    gathered (WrapDivisible x) (WrapDivisible y) = WrapDivisible (divided x y)
-instance Divisible f => Inplicative (WrappedDivisible f) where
+-- | Ignores the covariant part of 'gather'
+instance Divise f => Inply (WrappedDivisible f) where
+    gather _ g (WrapDivisible x) (WrapDivisible y) = WrapDivisible (divise g x y)
+    gathered (WrapDivisible x) (WrapDivisible y) = WrapDivisible (divised x y)
+-- | @'knot' _ = 'conquer'@
+instance (Divisible f, Divise f) => Inplicative (WrappedDivisible f) where
     knot _ = conquer
 
 -- | Wrap an 'Applicative' that is not necessarily an 'Apply'.
@@ -212,9 +223,11 @@ instance Invariant f => Invariant (WrappedApplicativeOnly f) where
     invmap f g (WrapApplicativeOnly x) = WrapApplicativeOnly (invmap f g x)
 instance (Applicative f, Invariant f) => Apply (WrappedApplicativeOnly f) where
     x <.> y = x <*> y
+-- | Ignores the contravariant part of 'gather'
 instance (Applicative f, Invariant f) => Inply (WrappedApplicativeOnly f) where
     gather f _ (WrapApplicativeOnly x) (WrapApplicativeOnly y) = WrapApplicativeOnly (liftA2 f x y)
     gathered (WrapApplicativeOnly x) (WrapApplicativeOnly y) = WrapApplicativeOnly (liftA2 (,) x y)
+-- | @'knot' = 'pure'@
 instance (Applicative f, Invariant f) => Inplicative (WrappedApplicativeOnly f) where
     knot = pure
 
@@ -233,16 +246,72 @@ instance Invariant f => Invariant (WrappedDivisibleOnly f) where
     invmap f g (WrapDivisibleOnly x) = WrapDivisibleOnly (invmap f g x)
 instance (Divisible f, Invariant f) => Divise (WrappedDivisibleOnly f) where
     divise g (WrapDivisibleOnly x) (WrapDivisibleOnly y) = WrapDivisibleOnly (divide g x y)
+-- | Ignores the covariant part of 'gather'
 instance (Divisible f, Invariant f) => Inply (WrappedDivisibleOnly f) where
     gather _ g (WrapDivisibleOnly x) (WrapDivisibleOnly y) = WrapDivisibleOnly (divide g x y)
     gathered (WrapDivisibleOnly x) (WrapDivisibleOnly y) = WrapDivisibleOnly (divided x y)
+-- | @'knot' _ = 'conquer'@
 instance (Divisible f, Invariant f) => Inplicative (WrappedDivisibleOnly f) where
     knot _ = conquer
 
-deriving via WrappedApplicativeOnly (MaybeT m) instance (Monad m, Invariant m) => Inply (MaybeT m)
-deriving via WrappedApplicativeOnly (MaybeT m) instance (Monad m, Invariant m) => Inplicative (MaybeT m)
-deriving via WrappedApplicativeOnly (ListT m) instance (Monad m, Invariant m) => Inply (ListT m)
-deriving via WrappedApplicativeOnly (ListT m) instance (Monad m, Invariant m) => Inplicative (ListT m)
+funzip :: Functor f => f (a, b) -> (f a, f b)
+funzip x = (fmap fst x, fmap snd x)
+
+instance Inply f => Inply (MaybeT f) where
+    gather f g (MaybeT x) (MaybeT y) = MaybeT $
+      gather (liftA2 f) (funzip . fmap g) x y
+instance Inplicative f => Inplicative (MaybeT f) where
+    knot x = MaybeT (knot (Just x))
+
+instance (Inply f, Semigroup w) => Inply (WriterT w f) where
+    gather f g (WriterT x) (WriterT y) = WriterT $
+      gather (\case (a, q) -> \case (b, r) -> (f a b, q <> r))
+             (\case (a, s) -> case g a of (b, c) -> ((b, s), (c, s)))
+             x y
+instance (Inplicative f, Monoid w) => Inplicative (WriterT w f) where
+    knot x = WriterT (knot (x, mempty))
+
+instance (Inply f, Semigroup w) => Inply (Strict.WriterT w f) where
+    gather f g (Strict.WriterT x) (Strict.WriterT y) = Strict.WriterT $
+      gather (\(~(a, q)) (~(b, r)) -> (f a b, q <> r))
+             (\(~(a, s)) -> let ~(b, c) = g a in ((b, s), (c, s)))
+             x y
+instance (Inplicative f, Monoid w) => Inplicative (Strict.WriterT w f) where
+    knot x = Strict.WriterT (knot (x, mempty))
+
+instance Inply f => Inply (ReaderT r f) where
+    gather f g (ReaderT x) (ReaderT y) = ReaderT $ \r ->
+      gather f g (x r) (y r)
+instance Inplicative f => Inplicative (ReaderT r f) where
+    knot x = ReaderT (\_ -> knot x)
+
+instance Inply f => Inply (ExceptT e f) where
+    gather f g (ExceptT x) (ExceptT y) = ExceptT $
+      gather (liftA2 f) (funzip . fmap g) x y
+instance Inplicative f => Inplicative (ExceptT e f) where
+    knot x = ExceptT (knot (Right x))
+
+instance Inply f => Inply (ErrorT e f) where
+    gather f g (ErrorT x) (ErrorT y) = ErrorT $
+      gather (liftA2 f) (funzip . fmap g) x y
+instance Inplicative f => Inplicative (ErrorT e f) where
+    knot x = ErrorT (knot (Right x))
+
+instance Inply f => Inply (ListT f) where
+    gather f g (ListT x) (ListT y) = ListT $
+      gather (liftA2 f) (funzip . fmap g) x y
+instance Inplicative f => Inplicative (ListT f) where
+    knot x = ListT (knot [x])
+
+deriving via WrappedFunctor (RWST r w s m) instance (Monad m, Bind m, Invariant m, Semigroup w) => Inply (RWST r w s m)
+deriving via WrappedFunctor (RWST r w s m) instance (Monad m, Bind m, Invariant m, Monoid w) => Inplicative (RWST r w s m)
+deriving via WrappedFunctor (Strict.RWST r w s m) instance (Monad m, Bind m, Invariant m, Semigroup w) => Inply (Strict.RWST r w s m)
+deriving via WrappedFunctor (Strict.RWST r w s m) instance (Monad m, Bind m, Invariant m, Monoid w) => Inplicative (Strict.RWST r w s m)
+deriving via WrappedFunctor (StateT s m) instance (Monad m, Bind m, Invariant m) => Inply (StateT s m)
+deriving via WrappedFunctor (StateT s m) instance (Monad m, Bind m, Invariant m) => Inplicative (StateT s m)
+deriving via WrappedFunctor (Strict.StateT s m) instance (Monad m, Bind m, Invariant m) => Inply (Strict.StateT s m)
+deriving via WrappedFunctor (Strict.StateT s m) instance (Monad m, Bind m, Invariant m) => Inplicative (Strict.StateT s m)
+
 deriving via WrappedApplicativeOnly (Tagged a) instance Inply (Tagged a)
 deriving via WrappedApplicativeOnly (Tagged a) instance Inplicative (Tagged a)
 
@@ -272,10 +341,6 @@ deriving via WrappedFunctor ((f Generics.:*: g) :: Type -> Type) instance (Apply
 deriving via WrappedFunctor ((f Generics.:*: g) :: Type -> Type) instance (Applicative f, Applicative g, Apply f, Apply g, Invariant f, Invariant g) => Inplicative (f Generics.:*: g)
 deriving via WrappedFunctor (Product f g :: Type -> Type) instance (Apply f, Apply g, Invariant f, Invariant g) => Inply (Product f g)
 deriving via WrappedFunctor (Product f g :: Type -> Type) instance (Applicative f, Applicative g, Apply f, Apply g, Invariant f, Invariant g) => Inplicative (Product f g)
-deriving via WrappedFunctor (((f :: Type -> Type) Generics.:.: g) :: Type -> Type) instance (Apply f, Apply g, Invariant f, Invariant g) => Inply (f Generics.:.: g)
-deriving via WrappedFunctor (((f :: Type -> Type) Generics.:.: g) :: Type -> Type) instance (Applicative f, Applicative g, Apply f, Apply g, Invariant f, Invariant g) => Inplicative (f Generics.:.: g)
-deriving via WrappedFunctor (Compose (f :: Type -> Type) g :: Type -> Type) instance (Apply f, Apply g, Invariant f, Invariant g) => Inply (Compose f g)
-deriving via WrappedFunctor (Compose (f :: Type -> Type) g :: Type -> Type) instance (Applicative f, Applicative g, Apply f, Apply g, Invariant f, Invariant g) => Inplicative (Compose f g)
 deriving via WrappedFunctor Complex instance Inply Complex
 deriving via WrappedFunctor Complex instance Inplicative Complex
 deriving via WrappedFunctor Semigroup.Min instance Inply Semigroup.Min
@@ -306,12 +371,6 @@ deriving via WrappedFunctor Tree instance Inply Tree
 deriving via WrappedFunctor Tree instance Inplicative Tree
 deriving via WrappedFunctor Seq instance Inply Seq
 deriving via WrappedFunctor Seq instance Inplicative Seq
-deriving via WrappedFunctor (ExceptT e m) instance (Monad m, Invariant m) => Inply (ExceptT e m)
-deriving via WrappedFunctor (ExceptT e m) instance (Monad m, Invariant m) => Inplicative (ExceptT e m)
-deriving via WrappedFunctor (ErrorT e m) instance (Monad m, Invariant m) => Inply (ErrorT e m)
-deriving via WrappedFunctor (ErrorT e m) instance (Monad m, Invariant m) => Inplicative (ErrorT e m)
-deriving via WrappedFunctor (RWST r w s m) instance (Monad m, Bind m, Invariant m, Semigroup w) => Inply (RWST r w s m)
-deriving via WrappedFunctor (RWST r w s m) instance (Monad m, Bind m, Invariant m, Monoid w) => Inplicative (RWST r w s m)
 deriving via WrappedFunctor (WrappedArrow a b) instance Arrow a => Inply (WrappedArrow a b)
 deriving via WrappedFunctor (WrappedArrow a b) instance Arrow a => Inplicative (WrappedArrow a b)
 deriving via WrappedFunctor (Generics.V1 :: Type -> Type) instance Inply Generics.V1
@@ -322,12 +381,6 @@ deriving via WrappedFunctor (Const w :: Type -> Type) instance Semigroup w => In
 deriving via WrappedFunctor (Const w :: Type -> Type) instance Monoid w => Inplicative (Const w)
 deriving via WrappedFunctor (Constant w :: Type -> Type) instance Semigroup w => Inply (Constant w)
 deriving via WrappedFunctor (Constant w :: Type -> Type) instance Monoid w => Inplicative (Constant w)
-deriving via WrappedFunctor ((,) w :: Type -> Type) instance Semigroup w => Inply ((,) w)
-deriving via WrappedFunctor ((,) w :: Type -> Type) instance Monoid w => Inplicative ((,) w)
-deriving via WrappedFunctor (WriterT w m) instance (Apply m, Invariant m, Semigroup w) => Inply (WriterT w m)
-deriving via WrappedFunctor (WriterT w m) instance (Applicative m, Apply m, Invariant m, Monoid w) => Inplicative (WriterT w m)
-deriving via WrappedFunctor (ReaderT r m) instance (Apply m, Invariant m) => Inply (ReaderT r m)
-deriving via WrappedFunctor (ReaderT r m) instance (Applicative m, Apply m, Invariant m) => Inplicative (ReaderT r m)
 deriving via WrappedFunctor (ContT r (m :: Type -> Type)) instance Inply (ContT r m)
 deriving via WrappedFunctor (ContT r (m :: Type -> Type)) instance Inplicative (ContT r m)
 deriving via WrappedFunctor (WrappedMonad m) instance Monad m => Inply (WrappedMonad m)
@@ -344,9 +397,24 @@ deriving via WrappedFunctor (Reverse f :: Type -> Type) instance (Apply f, Invar
 deriving via WrappedFunctor (Reverse f :: Type -> Type) instance (Applicative f, Apply f, Invariant f) => Inplicative (Reverse f)
 deriving via WrappedFunctor (Backwards f :: Type -> Type) instance (Apply f, Invariant f) => Inply (Backwards f)
 deriving via WrappedFunctor (Backwards f :: Type -> Type) instance (Applicative f, Apply f, Invariant f) => Inplicative (Backwards f)
+deriving via WrappedFunctor ((,) w :: Type -> Type) instance Semigroup w => Inply ((,) w)
+deriving via WrappedFunctor ((,) w :: Type -> Type) instance Monoid w => Inplicative ((,) w)
 
 deriving via WrappedFunctor (Lift f) instance (Invariant f, Apply f) => Inply (Lift f)
 instance (Invariant f, Apply f) => Inplicative (Lift f) where knot x = Pure x
+
+deriving via WrappedDivisible SettableStateVar instance Inply SettableStateVar
+deriving via WrappedDivisible SettableStateVar instance Inplicative SettableStateVar
+deriving via WrappedDivisible Predicate instance Inply Predicate
+deriving via WrappedDivisible Predicate instance Inplicative Predicate
+deriving via WrappedDivisible Comparison instance Inply Comparison
+deriving via WrappedDivisible Comparison instance Inplicative Comparison
+deriving via WrappedDivisible Equivalence instance Inply Equivalence
+deriving via WrappedDivisible Equivalence instance Inplicative Equivalence
+deriving via WrappedDivisible (Op r) instance Semigroup r => Inply (Op r)
+deriving via WrappedDivisible (Op r) instance Monoid r => Inplicative (Op r)
+
+
 
 
 -- | Convenient wrapper to build up an 'Inplicative' instance by providing
